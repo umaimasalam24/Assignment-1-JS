f097005c6ff0d023f0bc33c58acf6ea1c38007d4
Umaima Salam <umaimasalam2003@gmail.com>
2025-06-19 13:42:48 +0500

    Add files via upload

---
+++ assignment1.js
@@ -0,0 +1,13 @@
+// 1. Write a script to greet your website visitor using JS alert 
+// box.
+// alert("Hello! Welcome to our website");
+// 2. Write a script to display following message on your web 
+// page:
+// alert("Error! Please enter a valid password.")
+// 3. Write a script to display following message on your web page: (Hint : Use line break)
+// alert("Welcome to JS Land... \n Happy Coding!")
+// 4. Write a script to display following messages in sequence:
+// alert("Welcome to JS Land...");
+// alert("Happy Coding! \n Prevent this page from creating additionals dialogs.");
+// 5. Generate the following message through browser’s developer console:
+// alert("Hello... I can run JS through my web browser's console");
